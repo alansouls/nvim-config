d4ea39ce5a602afda2a93236b8774e41fa887007
Alan Maia <maiaalan1111@gmail.com>
2026-02-11 21:03:15 +0000

    map go to definition

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>d", function ()
+  vim.lsp.buf.definition()
+end)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
