58d0d2985451f68f2c5b3382636096e091155a28
Alan Maia <maiaalan1111@gmail.com>
2026-03-21 19:25:30 +0000

    format go files on save

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,7 +1,7 @@
 require "nvchad.autocmds"
 
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
-  pattern = {'*.h', '*.cpp'},
+  pattern = {'*.h', '*.cpp', '*.go', '*.cs', '*.lua'},
   callback = function ()
     vim.lsp.buf.format()
   end
